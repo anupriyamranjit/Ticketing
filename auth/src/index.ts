96a8e517810788d41fcdb4d807762c14174b2d05
Anupriyam Ranjit <anupriyamranjit@Anupriyams-Air.ht.home>
2022-09-03 20:58:36 -0400

    Writing auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { json } from 'body-parser'
+import mongoose from "mongoose";
+import cookieSession from "cookie-session"
+
 import 'express-async-errors';
 import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from './routes/signin';
@@ -9,8 +12,14 @@ import { errorHandler } from '../middlewares/error-handler';
 import { NotFoundError } from '../errors/not-found-error';
 
 
+
 const app = express();
+app.settings('trust proxy', true);
 app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 
 
 app.use(currentUserRouter)
@@ -24,7 +33,18 @@ app.all("*", async(req,res,next) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+    
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    } catch (err) {
+        console.error(err)
+    }
+    console.log("Connecting to auth mongo database");
+}
 
 app.listen(3000, () => {
     console.log("Listening on Port 3000!")
 })
+
+start();
