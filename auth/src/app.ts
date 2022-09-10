fcc1cd7be9bee1565bc9fb7648688ff6e519e6c2
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-09-10 00:13:18 -0400

    Stable auth

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,31 +1,33 @@
 import express from 'express';
-import { json } from 'body-parser'
-import cookieSession from "cookie-session"
 import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@aranjit_ticketing/common';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
-
-
 const app = express();
-app.enable('trust proxy');
-app.use(json())
-app.use(cookieSession({
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
     signed: false,
-    secure: true
-}))
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async(req,res,next) => {
-    throw new NotFoundError()
-})
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
-export { app }
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,22 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 import { app } from './app';
 
 const start = async () => {
-    if(!process.env.JWT_KEY){
-        throw new Error("JWT_KEY is not defined")
-    }
-    try {
-    
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    } catch (err) {
-        console.error(err)
-    }
-    console.log("Connecting to auth mongo database");
-}
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
-app.listen(3000, () => {
-    console.log("Listening on Port 3000!")
-})
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
